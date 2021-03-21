683bc2a34d90442633f08df986cc6b68d083d98f
hardik239 <18ce129@charusat.edu.in>
2021-03-21 15:41:00 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+    //AOS Library
+    AOS.init();
+});
+window.addEventListener("load", function(){
+    const loader = document.querySelector(".preloader");
+    loader.className += " hidden";
+});
